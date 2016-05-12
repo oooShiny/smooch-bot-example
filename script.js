4e24635191605baf3e222ac5853d3310cb79b6e0
oooShiny <arbrown83@gmail.com>
2016-05-12 09:42:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Andy? Just say HELLO to get started.')
+            return bot.say('Hey, you found AndyBot! Say HELLO to get started.')
                 .then(() => 'speak');
         }
     },
